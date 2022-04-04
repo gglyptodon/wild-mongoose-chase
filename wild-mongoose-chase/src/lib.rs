dbc6355ab5b1d2f891aff30f8ed938e61fd78c9e
alisandra <alisandra.denton@hhu.de>
2022-04-04 21:43:02 +0200

    a little harder yet

--- wild-mongoose-chase/src/lib.rs
+++ wild-mongoose-chase/src/lib.rs
@@ -6,4 +6,4 @@ pub const FRAME_DURATION: f32 = 75.0;
 pub const MONGOOSE_SPAWN_TIME: f32 = 20.;
 pub const BETWEEN_MONGOOSE_TIME: f32 = 90.;
 pub const MONGOOSE_WARN_TIME: f32 = 10.;
-pub const GRAIN_TO_WEEDS_TIME: f32 = 80.;
+pub const GRAIN_TO_WEEDS_TIME: f32 = 70.;
